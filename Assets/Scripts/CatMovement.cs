100afd51036b106de82c98e89f70114cda3adbe0
Ronfreddy <eonngyiyong@gmail.com>
2024-01-27 03:43:40 +0800

    Link Cat's movement into input system

--- Assets/Scripts/CatMovement.cs
+++ Assets/Scripts/CatMovement.cs
@@ -9,20 +9,22 @@ public class CatMovement : MonoBehaviour
     private Vector3 _movement;
     private float _speed = 5f;
     private Rigidbody _rigidbody;
+    private BoxCollider _boxCollider;
 
-    public Vector3 _boxSize;
+    private Vector3 _boxSize;
     public float _boxDistance;
     public LayerMask _groundLayer;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _boxCollider = GetComponent<BoxCollider>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _boxSize = new Vector3(_boxCollider.size.x * transform.localScale.x, 0.1f, _boxCollider.size.z * transform.localScale.z);
     }
 
     // Update is called once per frame
@@ -48,19 +50,14 @@ public class CatMovement : MonoBehaviour
     // WHY THIS WORKS WTF
     private bool IsGrounded()
     {
-        if(Physics.BoxCast(transform.position, _boxSize, Vector3.down, transform.rotation, _boxDistance, _groundLayer))
+        if(Physics.BoxCast(_boxCollider.bounds.center, _boxSize, Vector3.down, transform.rotation, _boxDistance, _groundLayer))
         {
             return true;
         }
         else
         {
+            Debug.Log("Not Grounded");
             return false;
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position + Vector3.down * _boxDistance, _boxSize);
     }
 }
