0e49b6f6e82d10b36f5a8b2675d7251287b770e5
DanyYap <124513296+DanyYap@users.noreply.github.com>
2024-01-27 01:49:08 +0800

    DestroyBreak1

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public GameObject fractured;
+    public float breakForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Empty
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ObjectDestroy();
+        }
+ 
+    }
+
+    void ObjectDestroy()
+    {
+        GameObject frac =Instantiate(fractured, transform.position, transform.rotation);
+        foreach (Rigidbody rb in frac.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * breakForce;
+            rb.AddForce(force);
+        }
+
+        Destroy(gameObject);
+    }
+}
